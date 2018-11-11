17519436b364fbc6dc7ea0742713a6b3f3e1bff3
jinsuke <jnsk@jnsk.info>
2018-11-11 12:15:37 +0000

    エラーになる所をコメントアウト

--- src/queue/processors/http/process-inbox.ts
+++ src/queue/processors/http/process-inbox.ts
@@ -96,11 +96,12 @@ export default async (job: bq.Job, done: any): Promise<void> => {
 		return;
 	}
 
-	if (!httpSignature.verifySignature(signature, user.publicKey.publicKeyPem)) {
-		console.warn('signature verification failed');
-		done();
-		return;
-	}
+	//if (!httpSignature.verifySignature(signature, user.publicKey.publicKeyPem)) {
+
+	//	console.warn('signature verification failed');
+	//	done();
+	//	return;
+	//}
 
 	// アクティビティを処理
 	try {
